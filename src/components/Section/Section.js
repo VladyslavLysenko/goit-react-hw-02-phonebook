46113525de7b735901e1946c8bb419c67c33efff
VladyslavLysenko <Dodog12362@gmail.com>
2022-12-13 17:36:05 +0200

    repaired

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const Section = ({title,children}) => {
- return (<Section> 
+ return (<section> 
     {title && <h2>{title}</h2>}
     {children}
- </Section>
+ </section>
    
   );
 }
